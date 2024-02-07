9963ee37c33ae2425ceaa960ef28cb887a282664
deluxesande <deluxesande@gmail.com>
2024-02-07 23:36:10 +0300

    (add): Update account reference in views.py

--- mpesa_api/views.py
+++ mpesa_api/views.py
@@ -7,7 +7,7 @@ def index(request):
     # Use a Safaricom phone number that you have access to, for you to be able to view the prompt.
     phone_number = '0714208354'
     amount = 1
-    account_reference = 'Deluxe Mpesa Site'
+    account_reference = 'Deluxe Mpesa'
     transaction_desc = 'Description'
     callback_url = 'https://darajambili.herokuapp.com/express-payment';
     response = cl.stk_push(phone_number, amount, account_reference, transaction_desc, callback_url)
