6e98c11d6c8aee644b25af8b449a9cc18f7aaa5a
deluxesande <deluxesande@gmail.com>
2024-02-08 18:51:02 +0300

    (Add): added the mpesa express route (Fix): Restructured the urls to include a default url to show endpoints

--- mpesa_api/urls.py
+++ mpesa_api/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.RootAPI.as_view(), name='index'),
     path('stk-push-callback/', views.STKPushCallBack.as_view(), name='stk_push_callback'),
     path('status/', views.MpesaExpressStatusView.as_view(), name='stk_push_status'),
+    path('payment/', views.MpesaTransactionPayment.as_view(), name='mpesa_payment'),
 ]
--- mpesa_api/views.py
+++ mpesa_api/views.py
@@ -14,13 +14,14 @@ from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv
 import json
 import requests
-import os
 from datetime import datetime
+import base64
 
 # Load the .env file
 load_dotenv()
 
 cl = MpesaClient()
+
 
 def generate_auth_token():
     """
@@ -33,67 +34,86 @@ def generate_auth_token():
 
     # format the response so as to get the token only
     formated_response = json.loads(response.text)
-    token = formated_response['access_token']
-    
+    token = formated_response["access_token"]
+
     return token
 
-class IndexView(APIView):
-    def get(self, request):
-        phone_number = "0742210044"
-        amount = 1
-        account_reference = "Deluxe Mpesa"
-        transaction_desc = "Description"
-        callback_url = "https://292b-102-215-12-244.ngrok-free.app/stk-push-callback/"
+def get_encoded_string():
+    """
+    Description:Generate the password for encrypting the request by base64 encoding BusinessShortcode, Passkey and Timestamp.
+    """
+    # Define your shortcode, passkey, and timestamp
+    shortcode = settings.MPESA_SHORTCODE
+    passkey = settings.MPESA_PASSKEY
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 
-        response = cl.stk_push(
-            phone_number, amount, account_reference, transaction_desc, callback_url
+    # Combine the shortcode, passkey, and timestamp
+    combined_string = f"{shortcode}{passkey}{timestamp}"
+
+    # Encode the combined string in base64
+    return base64.b64encode(combined_string.encode()).decode()
+
+class RootAPI(APIView):
+    """
+    Description:This is the root endpoint of the API\n
+    """
+
+    def get(self, request, *args, **kwargs):
+        return Response(
+            {
+                "message": "Welcome to the Mpesa API",
+                "endpoints": {
+                    "STK Push": "/payment/",
+                    "STK Push Callback": "/stk-push-callback/",
+                    "STK Push Status": "/status/",
+                },
+            },
+            status=status.HTTP_200_OK,
         )
-        data = json.loads(response.text)
-
-        # # Save the transaction details to the database
-        # MpesaTransaction.objects.create(
-        #     transaction_id=data["MerchantRequestID"],
-        #     amount=amount,
-        #     phone_number=phone_number,
-        #     account_reference=account_reference,
-        #     transaction_desc=transaction_desc,
-        # )
-
-        return Response(data)
-
 
 @method_decorator(csrf_exempt, name="dispatch")
 class STKPushCallBack(APIView):
     """
     Description:This is the endpoint where safaricom are going to send the callback after a transaction\n
-    Expected Responses are either success or error 
+    Expected Responses are either success or error
     """
-    def post(self,request,*args,**kwargs):
-        
+
+    def post(self, request, *args, **kwargs):
+
         # first check that the request is not empty
         if request.data:
             print(request.data)
 
             # get the result code
-            the_result_code = request.data['Body']['stkCallback']['ResultCode']
+            the_result_code = request.data["Body"]["stkCallback"]["ResultCode"]
 
             if the_result_code == 0:
                 # this means its successful save the response here
                 print("Success request")
-                print(request.data['Body']['stkCallback']['CallbackMetadata']['Item'][1]['Value']) 
-            
+                print(
+                    request.data["Body"]["stkCallback"]["CallbackMetadata"]["Item"][1][
+                        "Value"
+                    ]
+                )
+
             else:
-                #this means the payment was not success so save the error still 
+                # this means the payment was not success so save the error still
                 print("This is not a successful request")
 
+                merchant_request_id = request.data["Body"]["stkCallback"][
+                    "MerchantRequestID"
+                ]
+                checkout_request_id = request.data["Body"]["stkCallback"][
+                    "CheckoutRequestID"
+                ]
+                result_code = request.data["Body"]["stkCallback"]["ResultCode"]
+                result_description = request.data["Body"]["stkCallback"]["ResultDesc"]
 
-                merchant_request_id = request.data['Body']['stkCallback']['MerchantRequestID']
-                checkout_request_id = request.data['Body']['stkCallback']['CheckoutRequestID']
-                result_code = request.data['Body']['stkCallback']['ResultCode']
-                result_description = request.data['Body']['stkCallback']['ResultDesc']
-                
                 # first do a query and get to see if the merchant_request_id and  checkout_request_id do exist
-                the_transaction = MpesaTransaction.objects.filter(transaction_id=merchant_request_id,checkout_request_id=checkout_request_id).first()
+                the_transaction = MpesaTransaction.objects.filter(
+                    transaction_id=merchant_request_id,
+                    checkout_request_id=checkout_request_id,
+                ).first()
                 try:
                     print(the_transaction)
 
@@ -103,25 +123,26 @@ class STKPushCallBack(APIView):
                     the_transaction.status = "FAILED"
                     the_transaction.save()
 
-
-
-                
                 except MpesaTransaction.DoesNotExist:
                     print("multiple objects or doesnt exist")
-                    pass         
+                    pass
         else:
             pass
             # print("the request is not valid")
-        
+
         message = {
             "ResultCode": 0,
             "ResultDesc": "The service was accepted successfully",
-            "ThirdPartyTransID": "1234567890"
-            }
-        
-        return Response(message,status=status.HTTP_200_OK)
+            "ThirdPartyTransID": "1234567890",
+        }
+
+        return Response(message, status=status.HTTP_200_OK)
+
 
 class MpesaExpressStatusView(APIView):
+    """
+    Description:This is the endpoint to check the status of a transaction\n
+    """
     def get(self, request):
         transaction_id = "2ba8-4165-beca-292db11f9ef8381589"
         # Define the URL for the Transaction Status API
@@ -130,7 +151,7 @@ class MpesaExpressStatusView(APIView):
         # Define the headers for the request
         headers = {
             "Authorization": f"Bearer {generate_auth_token()}",
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         }
 
         # Define the payload for the request
@@ -145,6 +166,42 @@ class MpesaExpressStatusView(APIView):
             "QueueTimeOutURL": "https://292b-102-215-12-244.ngrok-free.app/stk-push-callback/",
             "Remarks": "Checking transaction status",
             "Occasion": "CheckTransactionStatus"
+        }
+
+        # Send the request
+        response = requests.post(url, headers=headers, json=payload)
+
+        # Return the response from the transaction status request
+        return Response(response.json())
+    
+class MpesaTransactionPayment(APIView):
+    """
+    Description:This is the endpoint to initialize a payment\n
+    """
+    def post(self, request):
+        """
+        Description:Endpoint to initialize payment \n
+        """
+        url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+
+        # Define the headers for the request
+        headers = {
+            "Authorization": f"Bearer {generate_auth_token()}",
+            "Content-Type": "application/json",
+        }
+
+        payload = {
+            "BusinessShortCode": settings.MPESA_SHORTCODE,
+            "Password": get_encoded_string(),
+            "Timestamp":datetime.now().strftime("%Y%m%d%H%M%S"),
+            "TransactionType": "CustomerPayBillOnline",
+            "Amount": "1",
+            "PartyA": "254742210044", # The phone number sending money
+            "PartyB": settings.MPESA_SHORTCODE,
+            "PhoneNumber": "254742210044",
+            "CallBackURL": "https://292b-102-215-12-244.ngrok-free.app/stk-push-callback/",
+            "AccountReference": "Deluxe Sande",
+            "TransactionDesc": "Test",
         }
 
         # Send the request
